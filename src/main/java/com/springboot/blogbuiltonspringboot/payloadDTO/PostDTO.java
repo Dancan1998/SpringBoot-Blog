c156cef12d3225c22f64a79ece273b411febe419
Dancan Chibole <dancankingstar@gmail.com>
2022-05-29 18:25:10 +0300

    Nesting

--- src/main/java/com/springboot/blogbuiltonspringboot/payloadDTO/PostDTO.java
+++ src/main/java/com/springboot/blogbuiltonspringboot/payloadDTO/PostDTO.java
@@ -2,10 +2,15 @@ package com.springboot.blogbuiltonspringboot.payloadDTO;
 
 import lombok.Data;
 
+import java.util.Set;
+
 @Data
 public class PostDTO {
     private Long id;
     private String title;
     private String content;
     private String description;
+
+    // returns comments in the post object -> nesting
+    private Set<CommentDTO> comments;
 }
